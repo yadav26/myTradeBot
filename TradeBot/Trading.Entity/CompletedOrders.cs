338ca965e3a4e0bd350c43129110748aadd37458
Anshul Yadav <anshul.yadav@amd.com>
2017-08-28 15:27:40 +0530

    Adding profit column in Completed Orders.

--- TradeBot/Trading.Entity/CompletedOrders.cs
+++ TradeBot/Trading.Entity/CompletedOrders.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaxCalculator;
 using Trading.Model;
 
 namespace Trading.Entity
@@ -10,12 +11,17 @@ namespace Trading.Entity
     public class CompletedOrders
     {
         public string Ticker { get; set; }
-
+        public float Profit { get; set; }
         public ActiveOrder OrderDetails;
         public CompletedOrders(ActiveOrder ao)
         {
             OrderDetails = ao;
             Ticker = ao.Ticker;
+            float pp = ao.GetPurchaseOrderObject().Purchased_Price;
+            float sp = ao.OrderSaleDetails.Sale_price;
+            int units = ao.GetPurchaseOrderObject().Units;
+            float tax = Formulas.getZerodha_Deductions(pp,sp,units);
+            Profit = Formulas.netProfit(pp, sp, units,tax);
         }
 
     }
