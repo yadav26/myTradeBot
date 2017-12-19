b56df7a931c3dd2f8295b71efb059c9365ec7757
Anshul Yadav <anshul.yadav@amd.com>
2017-12-19 13:55:10 +0530

    Left folder.

---
+++ TradeBot/ExchangePortal/PortalLibrary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangePortal
+{
+    public static class PortalLibrary
+    {
+        public static Random rand = new Random();
+
+        public static float GetPrice( float cp )
+        {
+
+            float retval = rand.Next( (int)(cp -10), (int)(cp +10) );
+            //float retval = (float)(float.MaxValue * 2.0 * (rand.NextDouble() - 0.5));
+
+            return retval;
+        }
+    }
+}
